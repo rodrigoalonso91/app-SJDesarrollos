fbeb3c1f61239bc24a16b45052fff2a2cfafc48a
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2022-12-09 17:21:51 -0300

    💄 Add salesman button

--- pages/Salesman.jsx
+++ pages/Salesman.jsx
@@ -1,5 +1,22 @@
 import { Navbar } from './components'
 import useUserData from "../src/UseUserData"
+import DataGrid from 'react-data-grid';
+import 'react-data-grid/lib/styles.css';
+
+const testColumns = [
+    { key: 'id', name: 'ID' },
+    { key: 'firstname', name: 'Nombre' },
+    { key: 'lastname', name: 'Apellido' },
+    { key: 'telefono', name: 'Teléfono' },
+    { key: 'email', name: 'Email' }
+]
+
+const testRows = [
+    { id: 0, firstname: 'Tony', lastname: 'Stark', telefono: '123456', email: 'tony@email.com' },
+    { id: 0, firstname: 'Steven', lastname: 'Strange', telefono: '123456', email: 'steven@email.com' },
+    { id: 0, firstname: 'Thor', lastname: 'Odinson', telefono: '123456', email: 'thor@email.com' },
+    { id: 0, firstname: 'Petter', lastname: 'Parker', telefono: '123456', email: 'petter@email.com' },
+];
 
 export const Salesman = () => {
 
@@ -8,7 +25,15 @@ export const Salesman = () => {
     return (
         <>
             <Navbar nickname={ user.nickname }/>
-            <h1>Grilla</h1>
+
+            <div className="contairner d-flex flex-row-reverse align-items-center">
+                <button
+                  className='btn btn-primary mb-3 me-3'
+                >
+                    + Vendedor</button>
+            </div>
+
+            <DataGrid columns={testColumns} rows={testRows} />
         </>
     )
 }
