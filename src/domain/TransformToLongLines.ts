de7e9b78cdf4983467c76c49a6cdcf7676d5c0b6
Facundo Garcia <facundogarcia1993@gmail.com>
2023-01-17 21:27:43 -0300

    fix neighborhood generation algorithm for most blocks

--- src/domain/TransformToLongLines.ts
+++ src/domain/TransformToLongLines.ts
@@ -1,25 +1,27 @@
 import {Coordinate} from "./types/Coordinate"
 import {Line} from "./types/Line"
-import {SimpleLine} from "./types/SimpleLine"
 import {Perimeter} from "./types/Perimeter"
+import {SimpleLine} from "./types/SimpleLine"
 import {coordinatesAreEqual, first, getOtherEnd, last, linesTouch, lineTouchesCoordinate} from "./utils/LineUtils"
 
-export default function transformToLongLines(perimeters: Array<Perimeter>, lines: Array<SimpleLine>) {
-	const longPerimeters = transformPerimeterToLongs(perimeters, lines)
-	const longBlocks = transformLinesToLongs(longPerimeters, [...lines])
-	return wea(longBlocks)
+export default function transformToLongLines(blocks: Array<Line>, inners: Array<SimpleLine>) {
+	//A block is an array of coordinates. We need to decompose it into lines that will be the sides of a lot.
+	const externals = segmentBlocks(blocks, inners)
+	//External lines are grouped with related internal lines, scoped by block.
+	const longBlocks = groupExternalsWithRelatedInners(externals, [...inners])
+
+	return longBlocks.map(createBlockLots).map(orderLines)
 }
 
-function transformPerimeterToLongs(perimeters: Array<Perimeter>, lines: Array<SimpleLine>) {
-	const perimetersAsLongLines: Array<Array<Line>> = []
+function segmentBlocks(perimeters: Array<Perimeter>, inners: Array<SimpleLine>) {
+	const results: Array<Lines> = []
 	for (const perimeter of perimeters) {
-
 		const firsts: Line = []
 		let current: Line = firsts
 		const longs = []
 		for (let i = 0; i < perimeter.length; i++) {
 			const coordinate = perimeter[i]
-			const touching = lines.filter(line => lineTouchesCoordinate(line, coordinate))
+			const touching = inners.filter(line => lineTouchesCoordinate(line, coordinate))
 
 			if (touching.length > 1) throw Error(`found more than one lot line touching a block perimeter coordinate: ${JSON.stringify(touching)}`)
 
@@ -31,93 +33,89 @@ function transformPerimeterToLongs(perimeters: Array<Perimeter>, lines: Array<Si
 
 		const lasts = longs[longs.length - 1]
 		longs[longs.length - 1] = [...lasts, ...firsts]
-		perimetersAsLongLines.push(longs)
+		results.push(longs)
 	}
-	return perimetersAsLongLines
+	return results
 }
 
-function transformLinesToLongs(perimeters: Array<Array<Line>>, simples: Array<SimpleLine>) {
-
+function groupExternalsWithRelatedInners(perimeters: Array<Array<Line>>, simples: Array<SimpleLine>) {
 	return perimeters.map(perimeter => {
-		const longs = perimeter.map(line => [first(line)])
+		const origins = perimeter.map(line => first(line))
 
-		let i = 0
-		while (i < longs.length) {
-			const long = longs[i]
-
-			while (true) {
-				const current = last(long)
-				const found = simples.filter(simple => lineTouchesCoordinate(simple, current))
-				simples = simples.filter(simple => !found.includes(simple))
-
-				if (found.length === 0) {
-				 break
-				} else if (found.length === 1) {
-					long.push(getOtherEnd(found[0], current))
-				} else {
-					const additions = found.map(line => getOtherEnd(line, current))
-					additions.forEach(addition => {longs.push([current, addition])})
-					break
-				}
-			}
-
-			i++
+		const longs: Array<Line> = []
+		while (origins.length > 0) {
+			const origin = origins.shift()!
+			if(longs.some(long => lineTouchesCoordinate(long, origin))) continue
+			longs.push(...xuxa({line: [origin], simples}))
 		}
 
-		console.log("perimeter", perimeter.length)
-		console.log("longs", longs.filter(x => x.length > 1).length)
 		return {perimeter, longs: longs.filter(x => x.length > 1)}
 	})
 }
 
-function wea(blocks: Array<{ perimeter: Array<Line>, longs: Array<Line> }>) {
-	const block_lots = blocks.map(block => {
-		const perimeter = [...block.perimeter]
-		const results: Array<Array<Line>> = []
-		//TODO contemplate edge cases
-		while (perimeter.length > 0) {
-			const inners = [...block.longs]//TODO Maybe template this
-			const external = perimeter.shift()!
-			const start = inners.find(x => lineTouchesCoordinate(x, first(external)))!
+function xuxa({line, simples}: { line: Line, simples: Array<SimpleLine>}): Array<Line> {
+	const current = last(line)
+	const found = simples.filter(simple => lineTouchesCoordinate(simple, current))
 
-			const result = fome([external, start], last(external), inners)
-			if (result !== null)
-				results.push(result)
-			console.log(result !== null ? "win" : "loose")
-		}
-		return results
-	})
+	if (found.length === 0) return [line]
+	const remaining = simples.filter(simple => !found.includes(simple))
 
-	const results =  block_lots.map(lots => {
-		return lots.map(lot => {
-			let remaining = [...lot]
-		//console.log(lot)
-			const chain = [...remaining.shift()!]
-			while (remaining.length) {
-				const link = remaining.find(line => lineTouchesCoordinate(line, last(chain)))
-				if (link === undefined) throw Error("Explosion")//TODO error
-				const ordered = coordinatesAreEqual(last(chain), first(link)) ? link : link.reverse()
-				remaining = remaining.filter(x => x !== link)
-				chain.push(...ordered)
-			}
+	if (found.length === 1) return xuxa({line: [...line, getOtherEnd(found[0], current)], simples: remaining})
 
-			return chain
-			//TODO Assert chain uroboros
-		})
+		const additions = found.map(line => getOtherEnd(line, current))
 
-	})
-
-	return results.map(lots => lots.filter(lot => lot.every(coordinate => coordinate !== undefined)))
+		return [
+			line,
+			...additions.flatMap(addition => xuxa({line: [current, addition], simples: remaining}))
+		]
 }
 
-function fome(path: Array<Line>, destination: Coordinate, options: Array<Line>): Array<Line> | null {
+function orderLines(lots: Array<Array<Line>>) {
+	const results = lots.map(lot => {
+		let remaining = [...lot]
+
+		const chain = [...remaining.shift()!]
+		while (remaining.length) {
+			const link = remaining.find(line => lineTouchesCoordinate(line, last(chain)))
+			if (link === undefined) throw Error("Explosion")//TODO error
+			const ordered = coordinatesAreEqual(last(chain), first(link)) ? link : link.reverse()
+			remaining = remaining.filter(x => x !== link)
+			chain.push(...ordered)
+		}
+
+		return chain
+		//TODO Assert chain uroboros
+	})
+
+	return results.filter(lot => lot.every(coordinate => coordinate !== undefined))
+}
+
+function getLotPerimeter(path: Array<Line>, destination: Coordinate, options: Array<Line>): Array<Line> | null {
 	const current = last(path)
-	if(lineTouchesCoordinate(current, destination)) return path
+	if (lineTouchesCoordinate(current, destination)) return path
 	const touching = options.filter(x => linesTouch(current, x))
 	const remaining = options.filter(x => !touching.includes(x))
 	for (const line of touching) {
-		const result = fome([...path, line], destination, remaining)
+		const result = getLotPerimeter([...path, line], destination, remaining)
 		if (result !== null) return result
 	}
 	return null
 }
+
+function createBlockLots(block: { perimeter: Array<Line>, longs: Array<Line> }) {
+	const perimeter = [...block.perimeter]
+	const results: Array<Array<Line>> = []
+	//TODO contemplate edge cases
+	while (perimeter.length > 0) {
+		const inners = [...block.longs]//TODO Maybe template this
+		const external = perimeter.shift()!
+		const start = inners.find(x => lineTouchesCoordinate(x, first(external)))!
+
+		const result = getLotPerimeter([external, start], last(external), inners)
+		if (result === null) throw Error("Explosion")
+		results.push(result)
+	}
+	return results
+}
+
+type Lines = Array<Line>
