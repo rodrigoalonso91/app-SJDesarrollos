8563d9b64267698664a86a9fd3f8fe8c2f67f324
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-02-02 20:32:58 -0300

    🎨 Extract constants

--- pages/Clients/index.jsx
+++ pages/Clients/index.jsx
@@ -1,34 +1,18 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import useUserData from "../../src/hooks/UseUserData"
+import useUserData from '../../src/hooks/UseUserData'
 import { Navbar } from '../../src/web/components'
 import { CustomGrid } from '../../src/web/components/layout/CustomGrid';
-
-import { useMemo } from 'react';
+import { BASIC_COLUMS } from '../../src/web/constants/colums'
 import { getCustomers } from '../../src/server';
 
 export const Clients = ({ customers }) => {
 
     const user = useUserData();
 
-    const columns = useMemo(
-        () => [
-          { header: 'Nombre', accessorKey: 'firstname' },
-          { header: 'Apellido', accessorKey: 'lastname' },
-          { header: 'Teléfono', accessorKey: 'phone' },
-          { header: 'Email', accessorKey: 'email' }
-        ],
-        [],
-    );
-    
     return (
         <>
             <Navbar nickname={ user.nickname }/>
-            <CustomGrid collection="clients" columns={columns} data={customers} />
-            {/* {
-                isFormActivated
-                ? <AddSalesmanForm />
-                : <CustomGrid collection="clients" columns={columns} data={customers} />
-            } */}
+            <CustomGrid collection="clients" columns={BASIC_COLUMS} data={customers} />
         </>
     )
 }
--- pages/Salesmen/index.jsx
+++ pages/Salesmen/index.jsx
@@ -1,28 +1,18 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import useUserData from "../../src/hooks/UseUserData"
 import { getSalesmen } from '../../src/server/salesmen/GetSalesmen'
-import { useMemo } from 'react';
 import { Navbar } from '../../src/web/components'
 import { CustomGrid } from '../../src/web/components/layout';
+import { BASIC_COLUMS } from '../../src/web/constants/colums'
 
 export const Salesmen = ({ salesmen }) => {
 
     const user = useUserData();
 
-    const columns = useMemo(
-        () => [
-          { header: 'Nombre', accessorKey: 'firstname' },
-          { header: 'Apellido', accessorKey: 'lastname' },
-          { header: 'Teléfono', accessorKey: 'phone' },
-          { header: 'Email', accessorKey: 'email' }
-        ],
-        [],
-    );
-
     return (
         <>
             <Navbar nickname={ user.nickname }/>
-            <CustomGrid collection="salesmen" columns={columns} data={salesmen} />
+            <CustomGrid collection="salesmen" columns={BASIC_COLUMS} data={salesmen} />
         </>
     )
 }
