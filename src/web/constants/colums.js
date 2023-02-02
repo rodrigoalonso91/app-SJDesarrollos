f6dd96ee21a194c2d83c9673466482fff93b835f
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-02-02 20:33:35 -0300

    🏷️ Add constants

---
+++ src/web/constants/colums.js
@@ -0,0 +1,6 @@
+export const BASIC_COLUMS = [
+    { header: 'Nombre', accessorKey: 'firstname' },
+    { header: 'Apellido', accessorKey: 'lastname' },
+    { header: 'Teléfono', accessorKey: 'phone' },
+    { header: 'Email', accessorKey: 'email' }
+];
