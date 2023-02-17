081fd1e5548a725e8f7733e177420afad2f5fb46
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-02-17 20:06:33 -0300

    💄 Update neigborhood page

--- pages/barrios/index.jsx
+++ pages/barrios/index.jsx
@@ -2,37 +2,60 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 import { MenuItem, TextField } from "@mui/material"
 import { Box } from "@mui/system"
 import { getNeighborhoods } from "@server/index"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { NeighborhoodGrid } from "../../src/web/components/layout/NeighborhoodGrid"
 
 export const Neighborhood = ({ neighborhoods }) => {
 
 	const [selectedNeighborhood, setSelectedNeighborhood] = useState('')
+	const [neighborhoodData, setNeighborhoodData] = useState({name: '', blocks: []})
+
+	useEffect(() => {
+
+		if (!selectedNeighborhood) return
+
+		const data = neighborhoods.find( n => n.id === selectedNeighborhood )
+		setNeighborhoodData(data)
+	},
+	[neighborhoods, selectedNeighborhood])
+	
 
 	const handleChange = (event) => {
 		setSelectedNeighborhood(event.target.value);
 	};
 	
 	return (
-		<Box width='250px'>
-			<TextField
-			  label='Elige un barrio'
-			  select
-			  value={selectedNeighborhood}
-			  onChange={handleChange}
-			  fullWidth
-			>
-				{
-					neighborhoods.map(n => 
-						<MenuItem  
-							key={n.id} 
-							value={n.id} 
-						>
-							{n.name}
-						</MenuItem>
-					)
-				}
-			</TextField>
-		</Box>
+		<section 
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				gap: '50px',
+				padding: 25
+			}}
+		>
+			<Box width='250px'>
+				<TextField
+					label='Elige un barrio'
+					select
+					value={selectedNeighborhood}
+					onChange={handleChange}
+					fullWidth
+				>
+					{
+						neighborhoods.map(n => 
+							<MenuItem  
+								key={n.id} 
+								value={n.id} 
+							>
+								{n.name}
+							</MenuItem>
+						)
+					}
+				</TextField>
+			</Box>
+			<NeighborhoodGrid data={neighborhoodData} />
+		</section>
 	)
 }
 
