031d3c38f35a89e81e410fb25966e86b721b7004
Facundo Garcia <facundogarcia-ae@team.asana.com>
2023-02-26 13:26:22 -0300

    :art: clean up code for neighborhood master

---
+++ src/web/components/master/KonvaErrorLines.tsx
@@ -0,0 +1,77 @@
+import { BlockError } from "@web/domain/TransformXmlToNeighborhoods"
+import { Coordinate } from "@web/domain/types/Coordinate"
+import React from "react"
+import { Line as KonvaLine } from "react-konva"
+
+export default function KonvaErrorLines({
+	errors
+}: {
+	errors: Array<BlockError>
+}) {
+	return (
+		<>
+			{errors &&
+				errors.map(({ lines }, i) =>
+					lines.map((line, j) => (
+						<ErrorLine
+							key={`${i}-${j}`}
+							coordinates={line}
+							highlighted={true}
+						/>
+					))
+				)}
+			{errors &&
+				errors.map((error, i) =>
+					(error.faulty || []).map((line, j) => (
+						<ErrorLineCulprit
+							key={`${i}-${j}`}
+							coordinates={line}
+							highlighted={true}
+						/>
+					))
+				)}
+		</>
+	)
+}
+
+function ErrorLine({
+	coordinates,
+	highlighted
+}: {
+	coordinates: Array<Coordinate>
+	highlighted: boolean
+}) {
+	const points = coordinates.flatMap((coordinate) => [
+		coordinate.x,
+		coordinate.y
+	])
+	return (
+		<KonvaLine
+			points={points}
+			fill="#00D2FF"
+			stroke={highlighted ? "green" : "black"}
+			strokeWidth={0.35}
+		/>
+	)
+}
+
+function ErrorLineCulprit({
+	coordinates,
+	highlighted
+}: {
+	coordinates: Array<Coordinate>
+	highlighted: boolean
+}) {
+	const points = coordinates.flatMap((coordinate) => [
+		coordinate.x,
+		coordinate.y
+	])
+	return (
+		<KonvaLine
+			points={points}
+			fill="#00D2FF"
+			stroke={highlighted ? "red" : "black"}
+			strokeWidth={highlighted ? 0.5 : 0.35}
+		/>
+	)
+}
---
+++ src/web/components/master/UseNeighborhood.ts
@@ -0,0 +1,77 @@
+import transformXmlToNeighborhoods, {
+	BlockError,
+	Neighborhood
+} from "@web/domain/TransformXmlToNeighborhoods"
+import { ChangeEvent, useCallback, useState } from "react"
+
+export default function useNeighborhood() {
+	const [neighborhood, setNeighborhood] = useState<Neighborhood | null>(null)
+	const [errors, setErrors] = useState<Array<BlockError> | null>(null)
+
+	async function onFileUpload(e: ChangeEvent<HTMLInputElement>) {
+		if (!e.target.files) return
+		const file = e.target.files[0]
+		const xml = await encode(file)
+
+		const { neighborhood, errors } = transformXmlToNeighborhoods(xml)
+		setNeighborhood(neighborhood)
+		setErrors(errors)
+	}
+
+	const changeLotName = useCallback(
+		(name: string, block: number, lot: number) => {
+			setNeighborhood((neighborhood) => {
+				if (neighborhood === null) return null
+				const substitute = clone(neighborhood)
+				substitute.blocks[block].lots[lot].name = name
+				return substitute
+			})
+		},
+		[neighborhood]
+	)
+
+	const changeBlockName = useCallback(
+		(name: string, block: number) => {
+			setNeighborhood((neighborhood) => {
+				if (neighborhood === null) return null
+				const substitute = clone(neighborhood)
+				substitute.blocks[block].name = name
+				return substitute
+			})
+		},
+		[neighborhood]
+	)
+
+	const changeNeighborhoodName = useCallback(
+		(name: string) => {
+			setNeighborhood((neighborhood) => {
+				if (neighborhood === null) return null
+				const substitute = clone(neighborhood)
+				substitute.name = name
+				return substitute
+			})
+		},
+		[neighborhood]
+	)
+
+	return {
+		onFileUpload,
+		neighborhood,
+		errors,
+		changeNeighborhoodName,
+		changeBlockName,
+		changeLotName
+	}
+}
+
+async function encode(file: File): Promise<string> {
+	const promise = new Promise<string>((resolve, reject) => {
+		const reader = new FileReader()
+		reader.onload = (ev) => resolve(ev.target!.result as string)
+		reader.onerror = reject
+		reader.readAsText(file, "UTF-8")
+	})
+	return await promise
+}
+
+const clone = <T>(obj: T) => JSON.parse(JSON.stringify(obj))
