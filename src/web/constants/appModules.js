8357b749f0f88a657c302cc02a6db443631637f8
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-02-26 16:15:41 -0300

    ✨ Add appModules

---
+++ src/web/constants/appModules.js
@@ -0,0 +1,27 @@
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import Diversity1Icon from '@mui/icons-material/Diversity1';
+import MapIcon from '@mui/icons-material/Map';
+
+export 	const modules = [
+    {
+        title: 'Master',
+        description: 'Importación de planos para crear barrios.',
+        icon: () => ( <MapIcon sx={{ color: '#1ea28f'}}/> )
+    },
+    {
+        title: 'Barrios',
+        description: 'Control y gestión de Barrios.',
+        icon: () => ( <DashboardIcon sx={{ color: '#1ea28f'}}/> )
+    },
+    {
+        title: 'Vendedores',
+        description: 'Administración de vendedores.',
+        icon: () => ( <HandshakeIcon sx={{ color: '#1ea28f'}}/> )
+    },
+    {
+        title: 'Clientes',
+        description: 'Administración de clientes.',
+        icon: () => ( <Diversity1Icon sx={{ color: '#1ea28f'}}/> )
+    },
+]
