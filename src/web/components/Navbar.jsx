350a38f794085b1137278353f169395b54bdf0a8
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-03-05 18:43:32 -0300

    💄 Remove margin bottom

--- src/web/components/Navbar.jsx
+++ src/web/components/Navbar.jsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
 
 	return (
 		<>
-			<nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2 mb-3">
+			<nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
 				<Link className="navbar-brand" href="/">
 					<Image
 						src="/logos/SJ-Logo-Wh.png"
