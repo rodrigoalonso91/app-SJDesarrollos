4fcaa71994b44288a4e7cc7b8ad675c72c48790d
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-03-12 16:08:06 -0300

    ✨ Add put method

--- pages/api/barrios/[id].ts
+++ pages/api/barrios/[id].ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import getUser from "@server/infrastructure/GetUser"
 import deleteNeighborhoodOfId from "@server/domain/neighborhood/DeleteNeighborhoodOfId"
+import { updateNeighborhood } from "@server/domain/neighborhood/UpdateNeighborhood"
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
+export default async function handler ( req: NextApiRequest, res: NextApiResponse ) {
+	
 	const user = await getUser({ req, res })
 	if (!user.isLoggedIn) return res.status(401).end()
 
@@ -18,6 +17,11 @@ export default async function handler(
 				if (!user.isAdmin) return res.status(403).end()
 				await deleteNeighborhoodOfId(id)
 				return res.status(204).end()
+			}
+			case "PUT": {
+				if (!user.isAdmin) return res.status(403).end()
+				await updateNeighborhood(id, req.body)
+				return res.status(200).end()
 			}
 			default:
 				res.status(405).end()
