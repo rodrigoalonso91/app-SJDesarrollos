c3f1e22d68957d2dbf9f9e711e7e3554b141bacb
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-03-12 17:52:32 -0300

    🐛 Fix array problem on edit

--- src/web/domain/utils/MergeObjects.js
+++ src/web/domain/utils/MergeObjects.js
@@ -1,50 +1,51 @@
-export default function mergeObjects (sourceObject, targetObject) {
+export default function mergeObjects(sourceObject, targetObject) {
 
     // Comprobar que ambos parámetros sean objetos
-    if (typeof sourceObject !== 'object' || typeof targetObject !== 'object') {
-      return sourceObject;
+    if (typeof sourceObject !== "object" || typeof targetObject !== "object") {
+        return sourceObject
     }
-    
+  
     // Crear un nuevo objeto para almacenar la fusión
     const merged = {};
-    
+  
     // Combinar las propiedades de ambos objetos
-
     for (let key in sourceObject) {
-
-      if (Object.prototype.hasOwnProperty.call(sourceObject, key)) {
-
-        if (targetObject[key] !== undefined && targetObject[key] !== null && targetObject[key] !== '') {
-
-          if (typeof sourceObject[key] === 'object' && typeof targetObject[key] === 'object') {
-
-            // Si ambas propiedades son objetos, fusionarlos recursivamente
-            merged[key] = mergeObjects(sourceObject[key], targetObject[key]);
-          } 
-          else {
-
-            // De lo contrario, utilizar la propiedad del segundo objeto
-            merged[key] = targetObject[key];
-          }
-        } 
-        else {
-          // Si la propiedad del segundo objeto es null, undefined o texto vacío,
-          // utilizar la propiedad del primer objeto
-          merged[key] = sourceObject[key];
+        if (Object.prototype.hasOwnProperty.call(sourceObject, key)) {
+            if (
+              targetObject[key] !== undefined &&
+              targetObject[key] !== null &&
+              targetObject[key] !== ""
+            ) {
+              if ( Array.isArray(sourceObject[key]) && Array.isArray(targetObject[key]) ) {
+  
+                // Si ambas propiedades son matrices, fusionarlas
+                merged[key] = [...sourceObject[key], ...targetObject[key]];
+              } 
+              else if ( typeof sourceObject[key] === "object" && typeof targetObject[key] === "object" ) {
+                // Si ambas propiedades son objetos, fusionarlos recursivamente
+                merged[key] = mergeObjects(sourceObject[key], targetObject[key]);
+              } 
+              else {
+                // De lo contrario, utilizar la propiedad del segundo objeto
+                merged[key] = targetObject[key];
+              }
+            } 
+            else {
+              // Si la propiedad del segundo objeto es null, undefined o texto vacío,
+              // utilizar la propiedad del primer objeto
+              merged[key] = sourceObject[key];
+            }
         }
-      }
     }
-    
+  
     // Agregar las propiedades del segundo objeto que no están presentes en el primero
     for (let key in targetObject) {
-
-      if (Object.prototype.hasOwnProperty.call(targetObject, key) && !Object.prototype.hasOwnProperty.call(sourceObject, key)) {
-
-        if (targetObject[key] !== undefined && targetObject[key] !== null && targetObject[key] !== '') {
-          merged[key] = targetObject[key];
+        if ( Object.prototype.hasOwnProperty.call(targetObject, key) && !Object.prototype.hasOwnProperty.call(sourceObject, key) ) {
+            if ( targetObject[key] !== undefined && targetObject[key] !== null && targetObject[key] !== "" ) {
+                merged[key] = targetObject[key];
+            }
         }
-      }
     }
-
+  
     return merged;
 }
