c8f3ab700ae487dee2311ca044ae28b22e5dc4bc
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-03-13 20:36:51 -0300

    🎨 Add Status colors

--- src/web/constants/lotStatus.js
+++ src/web/constants/lotStatus.js
@@ -2,4 +2,10 @@ export const STATUS_OPTIONS = [
     'Disponible',
     'Vendido',
     'Reservado'
-];
+]
+
+export const STATUS_COLORS = {
+    Disponible: '#0af2a1',
+    Vendido: '#e42929',
+    Reservado: '#decc00'
+}
