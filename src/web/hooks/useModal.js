2ec96ee4276bd973076137aa217c77a537788ca5
Rodrigo Alonso <rodrigo.alonso@urbetrack.com>
2023-03-12 21:36:09 -0300

    ⚡️ Add useModel

---
+++ src/web/hooks/useModal.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export function useModal ({ initialValue = false }) {
+
+    const [isModalOpen, setIsModalOpen] = useState()
+
+    useEffect(() => {
+        setIsModalOpen(initialValue)
+    }, [initialValue])
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    return {
+        isModalOpen,
+        openModal,
+        closeModal
+    }
+}
